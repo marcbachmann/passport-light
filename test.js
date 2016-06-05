1f1d29a32f51630fcbccd2ad134b2e5d93ae0981
Marc Bachmann <marc.brookman@gmail.com>
2016-06-05 13:36:17 +0200

    Use tape for tests

--- test.js
+++ test.js
@@ -1,88 +1,107 @@
-var assert = require('assert')
+var test = require('tape')
 var passport = require('./')
 
-function Strategy () {}
-Strategy.prototype.authenticate = function (req) {
-  this.error(new Error('something is wrong'))
-}
+test('error case', function (t) {
+  t.plan(1)
 
-passport.authenticate(new Strategy(), {
-  req: {},
-  success: assert.fail,
-  fail: assert.fail,
-  pass: assert.fail,
-  redirect: assert.fail,
-  error: function (err) {
-    assert.equal(err.message, 'something is wrong')
+  function Strategy () {}
+  Strategy.prototype.authenticate = function (req) {
+    this.error(new Error('something is wrong'))
   }
+
+  passport.authenticate(new Strategy(), {
+    req: {},
+    success: t.fail,
+    fail: t.fail,
+    pass: t.fail,
+    redirect: t.fail,
+    error: function (err) {
+      t.equal(err.message, 'something is wrong')
+    }
+  })
 })
 
-function Strategy2 () {}
-Strategy2.prototype.authenticate = function (req) {
-  this.fail('foobar', 401)
-}
+test('fail case', function (t) {
+  t.plan(2)
 
-passport.authenticate(new Strategy2(), {
-  req: {},
-  success: assert.fail,
-  pass: assert.fail,
-  redirect: assert.fail,
-  error: assert.fail,
-  fail: function (challenge, status) {
-    assert.equal(challenge, 'foobar')
-    assert.equal(status, 401)
+  function Strategy2 () {}
+  Strategy2.prototype.authenticate = function (req) {
+    this.fail('foobar', 401)
   }
+
+  passport.authenticate(new Strategy2(), {
+    req: {},
+    success: t.fail,
+    pass: t.fail,
+    redirect: t.fail,
+    error: t.fail,
+    fail: function (challenge, status) {
+      t.equal(challenge, 'foobar')
+      t.equal(status, 401)
+    }
+  })
 })
 
-function Strategy3 () {}
-var user = {id: 1, name: 'Foo'}
-var info = {foo: 'bar'}
-Strategy3.prototype.authenticate = function (req) {
-  this.success(user, info)
-}
+test('success case', function (t) {
+  t.plan(2)
 
-passport.authenticate(new Strategy3(), {
-  req: {},
-  pass: assert.fail,
-  redirect: assert.fail,
-  error: assert.fail,
-  fail: assert.fail,
-  success: function (loggedInUser, localInfo) {
-    assert.equal(user, loggedInUser)
-    assert.equal(info, localInfo)
+  function Strategy3 () {}
+  var user = {id: 1, name: 'Foo'}
+  var info = {foo: 'bar'}
+  Strategy3.prototype.authenticate = function (req) {
+    this.success(user, info)
   }
+
+  passport.authenticate(new Strategy3(), {
+    req: {},
+    pass: t.fail,
+    redirect: t.fail,
+    error: t.fail,
+    fail: t.fail,
+    success: function (loggedInUser, localInfo) {
+      t.equal(user, loggedInUser)
+      t.equal(info, localInfo)
+    }
+  })
 })
 
-function Strategy4 () {}
-var options = {foo: 'bar'}
-Strategy4.prototype.authenticate = function (req, localOptions) {
-  assert.equal(options, localOptions, 'Passes through the options')
-}
+test('options object', function (t) {
+  t.plan(1)
 
-passport.authenticate(new Strategy4(), {
-  req: {},
-  options: options,
-  success: assert.fail,
-  redirect: assert.fail,
-  error: assert.fail,
-  fail: assert.fail,
-  pass: assert.fail
+  function Strategy4 () {}
+  var options = {foo: 'bar'}
+  Strategy4.prototype.authenticate = function (req, localOptions) {
+    t.equal(options, localOptions, 'Passes through the options')
+  }
+
+  passport.authenticate(new Strategy4(), {
+    req: {},
+    options: options,
+    success: t.fail,
+    redirect: t.fail,
+    error: t.fail,
+    fail: t.fail,
+    pass: t.fail
+  })
 })
 
-function RedirectStrategy () {}
-RedirectStrategy.prototype.authenticate = function (req, localOptions) {
-  this.redirect('http://example.com', 302)
-}
+test('redirect', function (t) {
+  t.plan(2)
 
-passport.authenticate(new RedirectStrategy(), {
-  req: {},
-  options: options,
-  success: assert.fail,
-  error: assert.fail,
-  fail: assert.fail,
-  pass: assert.fail,
-  redirect: function (url, status) {
-    assert.equal(url, 'http://example.com')
-    assert.equal(status, 302)
+  function RedirectStrategy () {}
+  RedirectStrategy.prototype.authenticate = function (req, localOptions) {
+    this.redirect('http://example.com', 302)
   }
+
+  passport.authenticate(new RedirectStrategy(), {
+    req: {},
+    success: t.fail,
+    error: t.fail,
+    fail: t.fail,
+    pass: t.fail,
+    redirect: function (url, status) {
+      t.equal(url, 'http://example.com')
+      t.equal(status, 302)
+    }
+  })
 })
